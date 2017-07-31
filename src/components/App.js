7d0d58b4861e4ef9f298b7952b1f1dec7d92706f
Luca Pasquale <lgpasquale@gmail.com>
2017-07-31 08:53:56 +0200

    Removed MuiThemeProvider from App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {hashHistory, Router, Route, IndexRedirect} from 'react-router';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppLayout from './AppLayout';
 import LoginForm from './LoginForm';
 import AccountSettings from './AccountSettings';
@@ -11,35 +10,33 @@ import PageNotFound from './PageNotFound';
 class App extends React.Component {
   render () {
     return (
-      <MuiThemeProvider>
-        <Router history={this.props.history}>
-          <Route path='/' component={(props) =>
-            (<AppLayout title={this.props.title}
-              history={this.props.history}
-              adminGroup={this.props.adminGroup || 'admin'}
-              children={props.children} />)
-          }>
-            <IndexRedirect to={this.props.mainPath} />
-            <Route path={this.props.mainPath} component={this.props.mainComponent}>
-              {this.props.routes}
-            </Route>
-            <Route path='login' component={
-              () => (<LoginForm history={this.props.history}
-                authServerURL={this.props.authServerURL}
-              />)}
-            />
-            <Route path='account-settings' component={
-              RequireAuth(() => (<AccountSettings
-                authServerURL={this.props.authServerURL}
-              />), this.props.history)} />
-            <Route path='user-management' component={
-              RequireAuth(() => (<UserManagement
-                authServerURL={this.props.authServerURL}
-              />), this.props.history)} />
-            <Route path='*' component={PageNotFound} />
+      <Router history={this.props.history}>
+        <Route path='/' component={(props) =>
+          (<AppLayout title={this.props.title}
+            history={this.props.history}
+            adminGroup={this.props.adminGroup || 'admin'}
+            children={props.children} />)
+        }>
+          <IndexRedirect to={this.props.mainPath} />
+          <Route path={this.props.mainPath} component={this.props.mainComponent}>
+            {this.props.routes}
           </Route>
-        </Router>
-      </MuiThemeProvider>
+          <Route path='login' component={
+            () => (<LoginForm history={this.props.history}
+              authServerURL={this.props.authServerURL}
+            />)}
+          />
+          <Route path='account-settings' component={
+            RequireAuth(() => (<AccountSettings
+              authServerURL={this.props.authServerURL}
+            />), this.props.history)} />
+          <Route path='user-management' component={
+            RequireAuth(() => (<UserManagement
+              authServerURL={this.props.authServerURL}
+            />), this.props.history)} />
+          <Route path='*' component={PageNotFound} />
+        </Route>
+      </Router>
     );
   }
 }
